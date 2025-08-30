add64aa0fea765a7abc8cccc9e14cff64bea2eb2
francedirectjp-art <francedirectjp@gmail.com>
2025-08-30 23:33:58 +0900

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import os
+import swisseph as swe
+
+# Vercel環境でSwiss Ephemerisのデータファイルパスを設定
+# /var/task/swe_data にデータファイルを配置することを想定
+data_path = '/var/task/swe_data'
+if os.path.exists(data_path):
+    swe.set_ephe_path(data_path)
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import swisseph as swe
